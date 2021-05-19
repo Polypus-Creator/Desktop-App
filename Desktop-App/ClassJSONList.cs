f37047fdc119558732d7bfe8a493b50782b27ec1
Jose Santos <josesanpal@monlau.com>
2021-05-19 16:43:55 +0300

    Guardar fotos del usuario/JSON listo

---
+++ Desktop-App/ClassJSONList.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desktop_App
+{
+    class ClassJSONList
+    {
+        private List<JObject> listaJSON = new List<JObject>();
+
+        public ClassJSONList() { }
+
+        public List<JObject> ListaJSON { get => listaJSON; set => listaJSON = value; }
+    }
+}
